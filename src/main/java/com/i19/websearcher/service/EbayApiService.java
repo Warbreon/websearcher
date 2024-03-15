df8662450b820362eb2599a6d98fc6926273ea6e
Warbreon <w4rbreon@gmail.com>
2024-03-15 05:54:44 +0200

    refactoring: EbayApiService

--- src/main/java/com/i19/websearcher/service/EbayApiService.java
+++ src/main/java/com/i19/websearcher/service/EbayApiService.java
@@ -26,25 +26,9 @@ public class EbayApiService {
     public EbayTokenResponse refreshToken(String currentRefreshToken) {
         log.info("Processing: refreshToken");
 
-        String credentials = ebayApiConfig.getClientId() + ":" + ebayApiConfig.getClientSecret();
-        String encodedCredentials = Base64.getEncoder().encodeToString(credentials.getBytes(StandardCharsets.UTF_8));
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        headers.set("Authorization", "Basic " + encodedCredentials);
-
-        MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
-        requestBody.add("grant_type", "refresh_token");
-        requestBody.add("refresh_token", currentRefreshToken);
-        requestBody.add("scope", ebayApiConfig.getScope());
-
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(requestBody, headers);
-        ResponseEntity<EbayTokenResponse> response = restTemplate.exchange(
-                REFRESH_TOKEN_URL,
-                HttpMethod.POST,
-                request,
-                EbayTokenResponse.class
-        );
+        HttpHeaders headers = setupHeaders();
+        MultiValueMap<String, String> requestBody = setupRefreshTokenBody(currentRefreshToken);
+        ResponseEntity<EbayTokenResponse> response = processResponse(requestBody, headers);
 
         if(response.getStatusCode() == HttpStatus.OK && response.getBody() != null) {
             log.info("Token successfully refreshed. Access token: {}", response.getBody().getAccessToken());
@@ -59,28 +43,10 @@ public class EbayApiService {
         log.info("Processing: getNewAccessToken");
 
         String decodedAuthCode = URLDecoder.decode(ebayApiConfig.getAuthCode(), StandardCharsets.UTF_8);
-        log.debug("Decoded auth code: {}", decodedAuthCode);
 
-        String credentials = ebayApiConfig.getClientId() + ":" + ebayApiConfig.getClientSecret();
-        String encodedCredentials = Base64.getEncoder().encodeToString(credentials.getBytes(StandardCharsets.UTF_8));
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        headers.set("Authorization", "Basic " + encodedCredentials);
-
-        MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
-        requestBody.add("grant_type", "authorization_code");
-        requestBody.add("code", decodedAuthCode);
-        requestBody.add("redirect_uri", ebayApiConfig.getRuName());
-
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(requestBody, headers);
-
-        ResponseEntity<EbayTokenResponse> response = restTemplate.exchange(
-                REFRESH_TOKEN_URL,
-                HttpMethod.POST,
-                request,
-                EbayTokenResponse.class
-        );
+        HttpHeaders headers = setupHeaders();
+        MultiValueMap<String, String> requestBody = setupNewAccessTokenBody(decodedAuthCode);
+        ResponseEntity<EbayTokenResponse> response = processResponse(requestBody, headers);
 
         if (response.getStatusCode() == HttpStatus.OK) {
             log.info("Response from eBay API: {}", response.getBody());
@@ -92,5 +58,41 @@ public class EbayApiService {
             log.error("Failed to obtain tokens. Status code: {}, Response body: {}", response.getStatusCode(), response.getBody());
             throw new RuntimeException("Failed to obtain tokens: " + response.getStatusCode() + " Response body: " + response.getBody());
         }
+    }
+
+    private HttpHeaders setupHeaders() {
+        String credentials = ebayApiConfig.getClientId() + ":" + ebayApiConfig.getClientSecret();
+        String encodedCredentials = Base64.getEncoder().encodeToString(credentials.getBytes(StandardCharsets.UTF_8));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.set("Authorization", "Basic " + encodedCredentials);
+        return headers;
+    }
+
+    private MultiValueMap<String, String> setupRefreshTokenBody(String currentRefreshToken) {
+        MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
+        requestBody.add("grant_type", "refresh_token");
+        requestBody.add("refresh_token", currentRefreshToken);
+        requestBody.add("scope", ebayApiConfig.getScope());
+        return requestBody;
+    }
+
+    private MultiValueMap<String, String> setupNewAccessTokenBody(String decodedAuthCode) {
+        MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
+        requestBody.add("grant_type", "authorization_code");
+        requestBody.add("code", decodedAuthCode);
+        requestBody.add("redirect_uri", ebayApiConfig.getRuName());
+        return requestBody;
+    }
+
+    private ResponseEntity<EbayTokenResponse> processResponse(MultiValueMap<String, String> requestBody, HttpHeaders headers) {
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(requestBody, headers);
+        return restTemplate.exchange(
+                REFRESH_TOKEN_URL,
+                HttpMethod.POST,
+                request,
+                EbayTokenResponse.class
+        );
     }
 }
